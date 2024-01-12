a64d930e7f1b3f5bf6697b554fa9f07e64711817
sayed <sayedhassan800@gmail.com>
2024-01-12 23:19:47 +0200

    Add configuration for email service

---
+++ src/main/java/com/example/expenseTracker/util/email/EmailConfig.java
@@ -0,0 +1,8 @@
+package com.example.expenseTracker.util.email;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EmailConfig {
+
+}
---
+++ src/main/java/com/example/expenseTracker/util/email/EmailService.java
@@ -0,0 +1,30 @@
+package com.example.expenseTracker.util.email;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender emailSender;
+    @Async
+    public void sendEmail(String to, String subject, String text) {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+        try {
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text);
+            emailSender.send(message);
+        }
+        catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
