cb5dc4c2b90256ca04d2a9eec1eb61a8bc65c46a
sayed <sayedhassan800@gmail.com>
2024-01-12 23:25:08 +0200

    Generalize ErrorResponse

--- src/main/java/com/example/expenseTracker/exceptionHandling/errorResponse/ErrorResponse.java
+++ src/main/java/com/example/expenseTracker/exceptionHandling/errorResponse/ErrorResponse.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UnAuthorizedAccessErrorResponse {
+public class ErrorResponse {
     private int status;
     private String message;
     private LocalDateTime timeStamp;
